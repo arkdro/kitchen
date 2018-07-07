8ed430bb378f9a08a8dfdeb1fa2d2f326d49f026
arkdro <arkdro@gmail.com>
2018-07-08 00:04:31 +0200

    add direction to the mouse class

--- org/eff/kitchen/mouse.kt
+++ org/eff/kitchen/mouse.kt
@@ -1,7 +1,10 @@
 package org.eff.kitchen.mouse
 
+import org.eff.kitchen.direction.Direction
+
 abstract class Mouse() {
     abstract var x: Int
     abstract var y: Int
+    abstract var direction: Direction
     abstract var speed: Int
 }
