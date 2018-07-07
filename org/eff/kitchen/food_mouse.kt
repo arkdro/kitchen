ee2db08a1f7e51303e6c97da3c51c4a6e3dd4949
arkdro <arkdro@gmail.com>
2018-07-08 00:05:40 +0200

    create random coordinates with some slack

--- org/eff/kitchen/food_mouse.kt
+++ org/eff/kitchen/food_mouse.kt
@@ -3,12 +3,12 @@ package org.eff.kitchen.mouse
 import org.eff.kitchen.food.Food
 
 class Food_mouse(fodder: Food, width: Int, height: Int) : Mouse() {
-    override var x: Int = create_random(width)
-    override var y: Int = create_random(height)
+    override var x: Int = create_random_with_slack(width)
+    override var y: Int = create_random_with_slack(height)
     override var speed = 1
 }
 
-fun create_random(range: Int): Int {
+fun create_random_with_slack(range: Int): Int {
     val rnd = java.util.Random()
-    return rnd.nextInt(range)
+    return 1 + rnd.nextInt(range - 2)
 }
