6b470759a3753a84826175af45ea4fa9e00bb1b2
arkdro <arkdro@gmail.com>
2018-07-08 00:30:23 +0200

    work around for enum inheritance for directions

--- org/eff/kitchen/direction.kt
+++ org/eff/kitchen/direction.kt
@@ -1,7 +1,27 @@
 package org.eff.kitchen.direction
 
+abstract class Any_direction {
+    abstract val directions: Array<Direction>
+}
+
 enum class Direction {
     NW, N, NE,
     W,     E,
     SW, S, SE
+}
+
+class Normal_direction : Any_direction() {
+    override val directions = arrayOf(
+            Direction.N,
+            Direction.W,
+            Direction.E,
+            Direction.S)
+}
+
+class Diagonal_direction : Any_direction() {
+    override val directions = arrayOf(
+            Direction.NW,
+            Direction.NE,
+            Direction.SW,
+            Direction.SE)
 }
