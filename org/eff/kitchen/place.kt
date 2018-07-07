bb7e6183433166d962675d367224212c5cc5348d
arkdro <arkdro@gmail.com>
2018-07-07 23:49:27 +0200

    fill stock

--- org/eff/kitchen/place.kt
+++ org/eff/kitchen/place.kt
@@ -8,4 +8,13 @@ const val h_gap = 3
 class Place {
     val stock = Array<Array<Food>>
 
+fun Place.fill_stock() {
+    stock = Array(height, {Array(width, { Food.FULL })})
+    for (y in 0..height) {
+        for (x in 0..width) {
+            stock[y][x] = food_at_coordinates(x, y)
+        }
+    }
+}
+
 }
