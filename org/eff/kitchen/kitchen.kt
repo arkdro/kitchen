bd3ac4d0c32923faf7485675c6a333addd8eb6d3
arkdro <arkdro@gmail.com>
2018-07-13 22:28:26 +0200

    run the place

--- org/eff/kitchen/kitchen.kt
+++ org/eff/kitchen/kitchen.kt
@@ -1,10 +1,15 @@
 package org.eff.kitchen
 
+import org.eff.kitchen.place.Place
 import org.frice.Game
 import org.frice.launch
 
 class Kitchen : Game() {
 }
 
-fun main(args: Array<String>) = launch(Kitchen::class.java)
+// fun main(args: Array<String>) = launch(Kitchen::class.java)
+fun main(args: Array<String>) {
+    val place = Place()
+    place.run()
+}
 
