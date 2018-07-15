17537f3c7ede771565031ea3ff0400321aa91c0a
arkdro <arkdro@gmail.com>
2018-07-15 19:32:52 +0200

    onInit

--- org/eff/kitchen/kitchen.kt
+++ org/eff/kitchen/kitchen.kt
@@ -11,6 +11,29 @@ class Kitchen : Game() {
     init {
         logger.info("kitchen started")
     }
+    private val place = Place()
+    private var direction = KeyEvent.VK_SPACE // stop
+    override fun onInit() {
+        super.onInit()
+        val window_width = 300
+        val window_height = 200
+        setSize(window_width, window_height)
+        autoGC = false
+        isResizable = false
+        title = "The kitchen"
+        addKeyListener(object : KeyListener {
+            override fun keyTyped(e: KeyEvent) = Unit
+            override fun keyPressed(e: KeyEvent) {
+                val code = e.keyCode
+                if (is_allowed_key(code)) {
+                    logger.debug {"pressed: $e, ${e.keyChar}, ${e.keyCode}"}
+                    direction = code
+                }
+            }
+            override fun keyReleased(e: KeyEvent) = Unit
+        })
+
+    }
 }
 
 // fun main(args: Array<String>) = launch(Kitchen::class.java)
