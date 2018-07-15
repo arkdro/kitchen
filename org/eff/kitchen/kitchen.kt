e56f34b5cc2289756fdf435ccae4a8819432ebd4
arkdro <arkdro@gmail.com>
2018-07-15 19:32:05 +0200

    start logger on init

--- org/eff/kitchen/kitchen.kt
+++ org/eff/kitchen/kitchen.kt
@@ -1,10 +1,16 @@
 package org.eff.kitchen
 
+import mu.KotlinLogging
 import org.eff.kitchen.place.Place
 import org.frice.Game
 import org.frice.launch
 
+private val logger = KotlinLogging.logger {}
+
 class Kitchen : Game() {
+    init {
+        logger.info("kitchen started")
+    }
 }
 
 // fun main(args: Array<String>) = launch(Kitchen::class.java)
