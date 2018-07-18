232cfdaaa916ac70af7f64c3fca7e0e375b11d62
arkdro <arkdro@gmail.com>
2018-07-18 20:32:13 +0200

    implement subtraction for coordinates

--- src/main/kotlin/org/eff/kitchen/coordinates.kt
+++ src/main/kotlin/org/eff/kitchen/coordinates.kt
@@ -4,4 +4,7 @@ data class Coord(var x: Int, var y: Int) {
     operator fun plus(delta: Coord): Coord {
         return Coord(x + delta.x, y + delta.y)
     }
+    operator fun minus(delta: Coord): Coord {
+        return Coord(x - delta.x, y - delta.y)
+    }
 }
